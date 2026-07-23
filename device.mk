d56c5d945ae745c4509989e6c530208b205c5eb1
johnmart19 <johnivan19999@gmail.com>
2026-07-23 05:30:51 +0300

    redwood: Build libperfmgr for Lineage Power Service
    I init    : starting service 'vendor.power-hal-aidl'...
    F linker  : CANNOT LINK EXECUTABLE "/vendor/bin/hw/android.hardware.power-service.lineage-libperfmgr": library "libperfmgr.so" not found: needed by main executable

--- device.mk
+++ device.mk
@@ -411,7 +411,8 @@ PRODUCT_COPY_FILES += \
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service.lineage-libperfmgr \
-    libqti-perfd-client
+    libqti-perfd-client \
+    libperfmgr
 
 # Enable adpf cpu hint session for SurfaceFlinger and HWUI
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
